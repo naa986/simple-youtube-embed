6e9baaacab05bfa8f394f810bede7b6b222af042
naa986 <noorbd28@gmail.com>
2017-08-26 22:18:43 +0600

    .first commit

---
+++ simple-youtube-embed/main.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: Simple YouTube Embed
+Version: 1.0.5
+Plugin URI: http://noorsplugin.com/2014/07/17/simple-youtube-embed-plugin/
+Author: naa986
+Author URI: http://noorsplugin.com/
+Description: Embed YouTube video beautifully on your WordPress site
+Text Domain: simple-youtube-embed
+Domain Path: /languages 
+*/
+
+if(!defined('ABSPATH')) exit;
+if(!class_exists('SIMPLE_YOUTUBE_EMBED'))
+{
+    class SIMPLE_YOUTUBE_EMBED
+    {
+        var $plugin_version = '1.0.5';
+        var $plugin_url;
+        var $plugin_path;
+        function __construct()
+        {
+            define('SIMPLE_YOUTUBE_EMBED_VERSION', $this->plugin_version);
+            define('SIMPLE_YOUTUBE_EMBED_SITE_URL',site_url());
+            define('SIMPLE_YOUTUBE_EMBED_URL', $this->plugin_url());
+            define('SIMPLE_YOUTUBE_EMBED_PATH', $this->plugin_path());
+            $this->plugin_includes();
+            add_action( 'wp_enqueue_scripts', array( $this, 'plugin_scripts' ), 0 );
+        }
+        function plugin_includes()
+        {
+            if(is_admin( ) )
+            {
+                add_filter('plugin_action_links', array($this,'add_plugin_action_links'), 10, 2 );
+            }
+            add_action('plugins_loaded', array($this, 'plugins_loaded_handler'));
+            add_action('admin_menu', array( $this, 'add_options_menu' ));
+            add_action('wp_head', 'simple_youtube_video_embed_js');
+            add_filter('embed_oembed_html', 'simple_youtube_video_embed', 10, 3);
+        }
+        function plugin_scripts()
+        {
+            if (!is_admin()) 
+            {
+                wp_enqueue_script('jquery');
+                wp_register_script('waitforimages', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.waitforimages.min.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('waitforimages');
+                wp_register_script('prettyembed', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.prettyembed.min.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('prettyembed');
+                wp_register_script('fitvids', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.fitvids.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('fitvids');
+            }
+        }
+        function plugin_url()
+        {
+            if($this->plugin_url) return $this->plugin_url;
+            return $this->plugin_url = plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+        }
+        function plugin_path(){ 	
+            if ( $this->plugin_path ) return $this->plugin_path;		
+            return $this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
+        }
+        function add_plugin_action_links($links, $file)
+        {
+            if ( $file == plugin_basename( dirname( __FILE__ ) . '/main.php' ) )
+            {
+                $links[] = '<a href="options-general.php?page=simple-youtube-embed-settings">'.__('Settings', 'simple-youtube-embed').'</a>';
+            }
+            return $links;
+        }
+        
+        function plugins_loaded_handler()
+        {
+            load_plugin_textdomain('simple-youtube-embed', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/'); 
+        }
+
+        function add_options_menu()
+        {
+            if(is_admin())
+            {
+                add_options_page(__('Simple YouTube Embed', 'simple-youtube-embed'), __('Simple YouTube Embed', 'simple-youtube-embed'), 'manage_options', 'simple-youtube-embed-settings', array($this, 'display_options_page'));
+            }
+        }
+        
+        function display_options_page() 
+        {
+            $url = "https://noorsplugin.com/simple-youtube-embed-plugin/";
+            $link_text = sprintf(wp_kses(__('Please visit the <a target="_blank" href="%s">Simple YouTube Embed</a> documentation page for usage instructions.', 'simple-youtube-embed'), array('a' => array('href' => array(), 'target' => array()))), esc_url($url));
+            ?>
+            <div class="wrap"><h2>Simple YouTube Embed - v<?php echo $this->plugin_version; ?></h2>
+            <div class="update-nag"><?php echo $link_text;?></div>
+            </div>
+            <?php
+        }
+        
+    }
+    $GLOBALS['simple_youtube_embed'] = new SIMPLE_YOUTUBE_EMBED();
+}
+
+function simple_youtube_video_embed($html, $url, $attr) 
+{
+    $yt_pattern = "/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/";
+    if(preg_match($yt_pattern,$url,$matches))
+    {
+        $youtube_id = $matches[1];
+        $image_url = 'http://img.youtube.com/vi/'.$youtube_id.'/maxresdefault.jpg';
+        $response = wp_remote_request($image_url);
+        $response_code = wp_remote_retrieve_response_code($response);
+        $videoid = ' data-pe-videoid="'.$youtube_id.'"'; 
+        $fitvids = ' data-pe-fitvids="true"';
+        $previewsize = '';
+        if($response_code=="404"){
+            $previewsize = ' data-pe-preview-size="high"';
+        }
+        $showrelated = ''; 
+        $showcontrols = '';
+        $showinfo = '';
+        $allowFullScreen = '';
+        $parsed_url = parse_url($url); //parse the url to get query parameters
+        if(isset($parsed_url['query']) && !empty($parsed_url['query'])){
+            parse_str($parsed_url['query'], $data); //get query parameters into an array
+            if(isset($data['rel']) && $data['rel']=="0"){
+                $showrelated = ' data-pe-show-related="false"';
+            }
+            if(isset($data['controls']) && $data['controls']=="0"){
+                $showcontrols = ' data-pe-show-controls="false"';
+            }
+            if(isset($data['showinfo']) && $data['showinfo']=="0"){
+                $showinfo = ' data-pe-show-info="false"';
+            }
+            if(isset($data['fs']) && $data['fs']=="0"){
+                $allowFullScreen = ' data-pe-allow-fullscreen="false"';
+            }
+        }
+        $embed_code = <<<EOT
+        <div class="pretty-embed"{$videoid}{$fitvids}{$previewsize}{$showrelated}{$showcontrols}{$showinfo}{$allowFullScreen}></div>  
+EOT;
+        $html = $embed_code;
+    }
+    return $html; 
+}
+
+function simple_youtube_video_embed_js()
+{
+    $output = <<<EOT
+    <script type="text/javascript" charset="utf-8">
+        /* <![CDATA[ */
+        jQuery(document).ready(function($){
+            $(function(){
+                $().prettyEmbed({ useFitVids: true });
+            });
+        });
+        /* ]]> */
+        </script>
+EOT;
+    echo $output;
+}
---
+++ simple-youtube-embed/readme.txt
@@ -0,0 +1,110 @@
+=== Simple YouTube Embed ===
+Contributors: naa986
+Donate link: https://noorsplugin.com/
+Tags: video, embed, youtube, embed videos, embed youtube, embedding youtube, fluid, html5, iPad, iphone, mobile, simple, simple youtube, video plugin, wordpress youtube embed, youtube embed, youtube player, youtube plugin, responsive, shortcode, wordpress, you tube, youtube player, youtube-plugin
+Requires at least: 3.0
+Tested up to: 4.8
+Stable tag: 1.0.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed YouTube video in WordPress beautifully. Embed YouTube video with a URL or shortcode and customize the player using this YouTube embed plugin.
+
+== Description ==
+
+[Simple YouTube Embed](https://noorsplugin.com/simple-youtube-embed-plugin/) plugin is the easiest way to embed YouTube videos in WordPress. This plugin extends the default YouTube embed with advanced player parameters. When the page is rendered, a high resolution thumbnail image is displayed instead of the YouTube video player.
+
+= Features =
+
+* Pretty YouTube video embed
+* Responsive and mobile friendly
+* Embed a YouTube video with a high definition thumbnail image as the poster
+* Enable/Disable display of suggested videos when the video finishes
+* Enable/Disable player controls
+* Enable/Disable video information
+* Enable/Disable fullscreen
+
+= Simple YouTube Embed Usage =
+
+Create a new post/page and enter your YouTube video URL to embed it. Make sure the URL is on its own line and not hyperlinked (clickable when viewing the post). In order to avoid formatting errors, It's better to place the URL into the content area in text mode. For example:
+
+`http&#58;//www.youtube.com/watch?v=Vpg9yizPP_g`
+
+**Related YouTube Videos**
+
+In order to disable related videos from showing you can add "rel=0" to your YouTube URL.
+
+`http&#58;//www.youtube.com/watch?v=Vpg9yizPP_g&rel=0`
+
+**YouTube Video Controls**
+
+If you do not want to show player controls you can add "controls=0" to your YouTube URL.
+
+`http&#58;//www.youtube.com/watch?v=Vpg9yizPP_g&controls=0`
+
+**YouTube Video Info**
+
+If you do not want to show information like the video title and uploader you can add "showinfo=0" to your YouTube URL.
+
+`http&#58;//www.youtube.com/watch?v=Vpg9yizPP_g&showinfo=0`
+
+**YouTube Video Fullscreen**
+
+If you do not want to allow fullscreen option in the player you can add "fs=0" to your YouTube URL.
+
+`http&#58;//www.youtube.com/watch?v=Vpg9yizPP_g&fs=0`
+
+For documentation please visit the [Simple YouTube Embed](https://noorsplugin.com/simple-youtube-embed-plugin/) plugin page
+
+= Recommended Reading =
+
+* Simple YouTube Embed [Documentation](https://noorsplugin.com/simple-youtube-embed-plugin/)
+* My Other [Free WordPress Plugins](https://noorsplugin.com/wordpress-plugins/)
+
+== Installation ==
+
+1. Go to the Add New plugins screen in your WordPress Dashboard
+1. Click the upload tab
+1. Browse for the plugin file (simple-youtube-embed.zip) on your computer
+1. Click "Install Now" and then hit the activate button
+
+== Frequently Asked Questions ==
+
+= Can I use this plugin to embed a YouTube video with High Quality Thumbnail? =
+
+Yes.
+
+= Is this plugin responsive? =
+
+Yes.
+
+== Screenshots ==
+
+1. Simple YouTube Embed Demo
+
+== Upgrade Notice ==
+none
+
+== Changelog ==
+
+= 1.0.5 =
+* Compatible with WordPress 4.8
+* Updated all the permalinks
+
+= 1.0.4 =
+* Added an option to prevent video information from displaying in the YouTube player
+* Added an option to prevent fullscreen button from displaying in the YouTube player
+* Updated the translation files so the plugin can take advantage of language packs
+* Simple YouTube Embed is now compatible with WordPress 4.4
+
+= 1.0.3 =
+* Simple YouTube Embed is now compatible with WordPress 4.3
+
+= 1.0.2 =
+* Fixed "black bars" that would show up on the sides of the video and thumbnail
+* If high definition is not available for a video it will embed with a low quality thumbnail
+* Added an option to disable related videos
+* Added an option to disable player controls
+
+= 1.0.1 =
+* First commit
